01832ba292fdfed56920112841239ff58287088f
krishna-somineni <sominenisaikrishna1212@gmail.com>
2024-10-18 19:48:27 +0530

    project set up

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+ const Frst=()=>
+{
+    return (<>
+    <div>
+        <h4> Swiggy</h4>
+    </div>
+    </>);
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Frst/>);
